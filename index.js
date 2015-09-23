45f9958a3422267c48496e307ac746117892e683
Alexandr Ucstein <yksht@users.noreply.github.com>
2015-09-23 11:25:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,15 @@ var postcss = require('postcss');
 
 var definition = function (variables, override, node) {
     var name = node.prop.slice(1);
-    if ( override[name] === false ) {
+    if ( override[name] >= 0 ) {
         return;
     }
-    if ( node.value.indexOf('!const') != -1 ) {
-        override[name] = false;
-        variables[name] = node.value.replace('!const', '').trim();
-    } else {
-        override[name] = true;
-        variables[name] = node.value;
-    }
-    node.removeSelf();
+    override[name] = node.value.toString().indexOf('!const');
+    variables[name] = (override[name] === -1) ? node.value : node.value.toString().replace('!const', '').trim();
+    node.remove();
 };
 
-var variable = function (variables, node, str, name, opts) {
+var variable = function (variables, node, str, name, opts, result) {
     if ( opts.only ) {
         if ( typeof opts.only[name] !== 'undefined' ) {
             return opts.only[name];
@@ -30,44 +25,55 @@ var variable = function (variables, node, str, name, opts) {
         return str;
 
     } else {
-        throw node.error('Undefined variable $' + name);
+        var fix = opts.unknown(node, name, result);
+        if ( fix ) {
+            return fix;
+        } else {
+            return str;
+        }
     }
 };
 
-var simpleSyntax = function (variables, node, str, opts) {
-    return str.replace(/(^|[^\w])\$([\w\d-_]+)/g, function (_, before, name) {
-        return before + variable(variables, node, '$' + name, name, opts);
+var simpleSyntax = function (variables, node, str, opts, result) {
+    return str.replace(/(^|[^\w])\$([\w\d-_]+)/g, function (_, bef, name) {
+        return bef + variable(variables, node, '$' + name, name, opts, result);
     });
 };
 
-var inStringSyntax = function (variables, node, str, opts) {
+var inStringSyntax = function (variables, node, str, opts, result) {
     return str.replace(/\$\(\s*([\w\d-_]+)\s*\)/g, function (all, name) {
-        return variable(variables, node, all, name, opts);
+        return variable(variables, node, all, name, opts, result);
     });
 };
 
-var bothSyntaxes = function (variables, node, str, opts) {
-    str = simpleSyntax(variables, node, str, opts);
-    str = inStringSyntax(variables, node, str, opts);
+var bothSyntaxes = function (variables, node, str, opts, result) {
+    str = simpleSyntax(variables, node, str, opts, result);
+    str = inStringSyntax(variables, node, str, opts, result);
     return str;
 };
 
-var declValue = function (variables, node, opts) {
-    node.value = bothSyntaxes(variables, node, node.value, opts);
+var declValue = function (variables, node, opts, result) {
+    node.value = bothSyntaxes(variables, node, node.value, opts, result);
 };
 
-var ruleSelector = function (variables, node, opts) {
-    node.selector = bothSyntaxes(variables, node, node.selector, opts);
+var ruleSelector = function (variables, node, opts, result) {
+    node.selector = bothSyntaxes(variables, node, node.selector, opts, result);
 };
 
-var atruleParams = function (variables, node, opts) {
-    node.params = bothSyntaxes(variables, node, node.params, opts);
+var atruleParams = function (variables, node, opts, result) {
+    node.params = bothSyntaxes(variables, node, node.params, opts, result);
 };
 
 module.exports = postcss.plugin('postcss-simple-vars', function (opts) {
     if ( typeof opts === 'undefined' ) opts = { };
 
-    return function (css) {
+    if ( !opts.unknown ) {
+        opts.unknown = function (node, name) {
+            throw node.error('Undefined variable $' + name);
+        };
+    }
+
+    return function (css, result) {
         var variables = { };
         var override = { };
         if ( typeof opts.variables === 'function' ) {
@@ -76,11 +82,19 @@ module.exports = postcss.plugin('postcss-simple-vars', function (opts) {
             for ( var i in opts.variables ) variables[i] = opts.variables[i];
         }
 
-        css.eachInside(function (node) {
+        for ( var name in variables ) {
+            if ( name[0] === '$' ) {
+                var fixed = name.slice(1);
+                variables[fixed] = variables[name];
+                delete variables[name];
+            }
+        }
+
+        css.walk(function (node) {
 
             if ( node.type === 'decl' ) {
                 if ( node.value.toString().indexOf('$') !== -1 ) {
-                    declValue(variables, node, opts);
+                    declValue(variables, node, opts, result);
                 }
                 if ( node.prop[0] === '$' ) {
                     if ( !opts.only ) definition(variables, override, node);
@@ -88,12 +102,12 @@ module.exports = postcss.plugin('postcss-simple-vars', function (opts) {
 
             } else if ( node.type === 'rule' ) {
                 if ( node.selector.indexOf('$') !== -1 ) {
-                    ruleSelector(variables, node, opts);
+                    ruleSelector(variables, node, opts, result);
                 }
 
             } else if ( node.type === 'atrule' ) {
                 if ( node.params && node.params.indexOf('$') !== -1 ) {
-                    atruleParams(variables, node, opts);
+                    atruleParams(variables, node, opts, result);
                 }
             }
 
